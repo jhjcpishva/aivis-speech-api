aa957c46c913d64bfd975ce27b5c83bc9da500d1
jhjcpishva <jhjcpishva@users.noreply.github.com>
2025-02-22 18:41:14 +0900

    first fastapi

--- main.py
+++ main.py
@@ -1,6 +1,68 @@
-def main():
-    print("Hello from aivisspeechfastapi!")
+import io
+import logging
+import wave
+from enum import Enum
+
+import httpx
+import lameenc
+from fastapi import FastAPI
+from fastapi.responses import Response
 
 
-if __name__ == "__main__":
-    main()
+logger = logging.getLogger('uvicorn.app')
+speaker_id = "888753760"
+
+app = FastAPI()
+
+
+class AudioFormat(str, Enum):
+    WAV = "wav"
+    MP3 = "mp3"
+    
+
+def convert_to_mp3(audio_content: bytes) -> bytes:
+    with wave.open(io.BytesIO(audio_content), 'rb') as wf:
+        params = wf.getparams()
+        frames = wf.readframes(wf.getnframes())
+
+    encoder = lameenc.Encoder()
+    encoder.silence()
+    encoder.set_bit_rate(64)
+    encoder.set_quality(7)  # 2 best, 7 fast
+    encoder.set_in_sample_rate(params.framerate)
+    encoder.set_channels(params.nchannels)
+    encoded = encoder.encode(frames) + encoder.flush()
+    return encoded
+
+
+@app.get("/speech")
+async def speech(
+    text: str="吾輩は猫である。名前はまだ無い",
+    volume: float = 0.5,
+    pitch: float = 0,
+    speed: float = 1,
+    type: AudioFormat = AudioFormat.MP3):
+
+    logger.info(f"🐶 Request {text = } {volume = } {pitch = } {speed = } {type = }")
+    # AivisSpeech で音声を作成する
+    async with httpx.AsyncClient() as client:
+        audio_query = (await client.post(f"http://localhost:10101/audio_query?text={text}&speaker={speaker_id}")).json()
+        audio_query["volumeScale"] = volume
+        audio_query["pitchScale"] = pitch
+        audio_query["speedScale"] = speed
+        audio = await client.post(f"http://localhost:10101/synthesis?speaker={speaker_id}", json=audio_query, timeout=None)
+    logger.info(f"🐶 TTS Complete. wav size: {len(audio.content)/1024:.1f} KB")
+
+    if type == AudioFormat.WAV:
+        return Response(audio.content, media_type="audio/wav")
+
+    encoded = convert_to_mp3(audio.content)  # convert to mp3
+    logger.info(f"🐶 MP3 Encoded. mp3 size: {len(encoded)/1024:.1f}KB")
+
+    return Response(io.BytesIO(encoded).getvalue(), media_type="audio/mpeg")
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
