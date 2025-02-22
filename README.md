494f89e101868a40bea15bdcda4e4d4fa2ee1f0d
jhjcpishva <jhjcpishva@users.noreply.github.com>
2025-02-22 23:01:38 +0900

    add support .env

---
+++ README.md
@@ -0,0 +1,13 @@
+# AivisSpeechFastAPI
+
+
+## .env
+
+```.env
+AIVIS_ENGINE_URL="http://localhost:10101/"
+AIVIS_ENGINE_SPEAKER_ID="888753760"
+
+PORT="8000"
+CONTEXT_PATH="/"
+
+```
--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import httpx
 from fastapi import FastAPI
 from fastapi.responses import Response
 
+import config
 from convert_to_mp3 import convert_to_mp3
 from split_sentence_for_tts import split_sentence_for_tts
 
 logger = logging.getLogger('uvicorn.app')
-speaker_id = "888753760"
 
 app = FastAPI()
 
@@ -20,25 +20,26 @@ class AudioFormat(str, Enum):
     MP3 = "mp3"
     
 
-@app.get("/speech")
+@app.get(f"{config.CONTEXT_PATH}synthesis")
 async def speech(
     text: str="吾輩は猫である。名前はまだ無い",
     volume: float = 0.5,
     pitch: float = 0,
     speed: float = 1,
-    type: AudioFormat = AudioFormat.MP3):
+    speaker: str = config.AIVIS_ENGINE_SPEAKER_ID,
+    format: AudioFormat = AudioFormat.MP3):
 
-    logger.info(f"🐶 Request {text = } {volume = } {pitch = } {speed = } {type = }")
+    logger.info(f"🐶 Request {text = } {volume = } {pitch = } {speed = } {format = }")
     # AivisSpeech で音声を作成する
     async with httpx.AsyncClient() as client:
-        audio_query = (await client.post(f"http://localhost:10101/audio_query?text={text}&speaker={speaker_id}")).json()
+        audio_query = (await client.post(f"{config.AIVIS_ENGINE_URL}/audio_query?text={text}&speaker={speaker}")).json()
         audio_query["volumeScale"] = volume
         audio_query["pitchScale"] = pitch
         audio_query["speedScale"] = speed
-        audio = await client.post(f"http://localhost:10101/synthesis?speaker={speaker_id}", json=audio_query, timeout=None)
-    logger.info(f"🐶 TTS Complete. wav size: {len(audio.content)/1024:.1f} KB")
+        audio = await client.post(f"{config.AIVIS_ENGINE_URL}/synthesis?speaker={speaker}", json=audio_query, timeout=None)
+    logger.info(f"🐶 TTS Complete. wav size: {len(audio.content)/1024:.1f}KB")
 
-    if type == AudioFormat.WAV:
+    if format == AudioFormat.WAV:
         return Response(audio.content, media_type="audio/wav")
 
     encoded = convert_to_mp3(audio.content)  # convert to mp3
@@ -46,7 +47,7 @@ async def speech(
 
     return Response(io.BytesIO(encoded).getvalue(), media_type="audio/mpeg")
 
-@app.get("/split_sentence")
+@app.get(f"{config.CONTEXT_PATH}split_sentence")
 async def split_sentence(text: str):
     sentences = split_sentence_for_tts(text)
     for sentence in sentences:
@@ -57,4 +58,4 @@ async def split_sentence(text: str):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=config.PORT, reload=True)
